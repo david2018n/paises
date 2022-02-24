78640748b9f1485fea2f38e66c0055947cfc888e
JUAN DAVID NUSTES <juan_d500@hotmail.com>
2022-02-23 20:02:57 -0500

    se agrega pais detalle

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PaisService } from '../../services/pais.service';
+import { switchMap, tap } from 'rxjs/operators';
+import { Country } from '../../interfaces/pais-interface';
 
 @Component({
   selector: 'app-ver-pais',
@@ -8,9 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerPaisComponent implements OnInit {
 
-  constructor() { }
+  pais!: Country;
+  constructor( 
+      private activedRoute: ActivatedRoute,
+      private paisService:PaisService) { }
 
   ngOnInit(): void {
+    this.activedRoute.params
+    .pipe(
+      switchMap(({id}) => this.paisService.getPaisAlpha(id)),
+      tap(console.log)
+    )
+    .subscribe(pais =>{
+      this.pais=pais
+    })
+
+    // this.activedRoute.params
+    // .subscribe(({id}) =>{
+    //   console.log(id);
+    //   this.paisService.getPaisAlpha( id )
+    //   .subscribe(pais => {
+    //     console.log(pais)
+    //   }) 
+    // })
   }
 
 }
